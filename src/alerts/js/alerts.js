5294b59124178ee7dd183ee58e3c7d62245373e9
Narcis Radu <contact@narcisradu.ro>
2013-11-20 17:45:58 +0200

    Update alerts.js

--- src/alerts/js/alerts.js
+++ src/alerts/js/alerts.js
@@ -3,8 +3,8 @@
 		return $(this).on("click", function(e) {
 			e.preventDefault();
 			$(this).parent().fadeOut();
-		})
-	}
+		});
+	};
 
 	$('.pure-alert > a[rel="pure-alert-dismiss"]').pure_alert();
 }(jQuery));
